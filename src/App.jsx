e29bd036e6b35635c0b9789d5ada17142e319fcf
mizuki-yamada <my79909369@gmail.com>
2023-05-14 16:46:32 +0900

    ColorfulMessage.jsxのpropsを分割代入に書き換えた App.jsxでuseEffectを設定し、監視する変数をnumのみに指定した

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
-import React, { useState } from "react";
+/* react - hooks / exhaustive - deps; off */
+
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(10);
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  //numだけに関心を寄せたい処理があるとき
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfulMessage color="red">お元気ですか</ColorfulMessage>
       <ColorfulMessage color="pink">aaaaaaa</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>😃</p>}
     </>
   );
 };
